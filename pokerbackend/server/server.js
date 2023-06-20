8b4f51a4cb1133f0b35d725008566d8639ac69cd
Toshken21 <anton.rader@gmail.com>
2023-06-20 20:44:56 +0200

    prelaunch commit

--- pokerbackend/server/server.js
+++ pokerbackend/server/server.js
@@ -2,72 +2,56 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 
+// Import the schemas
+const betaAccountSchema = require("./betaAccountsModel");
+const lightroomArticleSchema = require("./lightroomArticleModel");
+const lightroomImageSchema = require("./lightroomImagesModel");
+
 app.use(cors());
+app.use(express.json());
 require("dotenv").config({path: "./config.env"});
 const mongoose = require('mongoose');
 
-//URI key
-const mongoURI = process.env.ATLAS_URI;
-console.log(mongoURI);
+//URI keys
+const mongoURI1 = process.env.ATLAS_URI1;
+const mongoURI2 = process.env.ATLAS_URI2;
 
-// Connect to the database
-mongoose.connect(mongoURI, {
+// Connect to the databases
+const conn1 = mongoose.createConnection(mongoURI1, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-    .then(console.log("Connected to MongoDb"))
-    .catch(err => console.log(err));
-
-
-
-
-// Define a schema for the data in the "accounts" collection
-const accountSchema = new mongoose.Schema({
-  name: { 
-    type: String,
-    required: true,
-    unique: true,
-    min: 2,
-    max: 24,
-
-},
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    max: 200,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 8,
-    max: 30,}
 });
 
-//we're going to validate the json
+const conn2 = mongoose.createConnection(mongoURI2, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+// Create models with specific connections
+const BetaAccount = conn1.model('BetaAccount', betaAccountSchema);
+const LightRoomArticle = conn2.model('LightRoomArticle', lightroomArticleSchema);
+const LightroomImage = conn2.model("LightRoomImage", lightroomImageSchema)
+;
 
+// Import routes and pass in models
+const betaAccountRoutes = require("./routes/betaAccountsRoutes")(BetaAccount);
+app.use("/beta", betaAccountRoutes);
 
-// Define a model for the "accounts" collection using the schema
-const Account = mongoose.model('Account', accountSchema);
+const lightRoomArticleRoutes = require("../server/routes/lightroomRoutes/lightroomRoutes")(LightRoomArticle);
+app.use("/lightroom", lightRoomArticleRoutes);
 
-// Example route that finds all accounts in the collection and sends them as JSON
-
+const lightRoomImageRoutes = require("../server/routes/lightroomRoutes/lightroomImageRoutes")(LightroomImage);
+app.use("/lightroomimage", lightRoomImageRoutes);
 
 // Start the server
-
-
 const server = app.listen(4000, () => {
     console.log('Server started on port 4000');
-  });
+});
 
 server.on("listening", () => {
     console.log("Server is listening");
 });
 
-server.on("error", () => {
+server.on("error", (error) => {
     console.error("Error starting server", error.message);
 });
-module.exports = Account;
-
